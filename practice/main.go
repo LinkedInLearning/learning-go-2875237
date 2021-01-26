5a1720dd7708300aaf076bd85bc0a8b797dbe7cb
David Gassner <david.gassner@gmail.com>
2021-01-26 14:56:36 -0800

    02_03e

--- practice/main.go
+++ practice/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	var aString string = "This is Go!"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered:", input)
 
 }
