7eea25347cca17cae29b2ccdbd947e084d7cbf06
David Gassner <david.gassner@gmail.com>
2021-01-26 15:10:08 -0800

    02_05b

--- practice/main.go
+++ practice/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	input, _ := reader.ReadString('\n')
-	fmt.Println("You entered:", input)
+	fmt.Println("Math")
 
 }
