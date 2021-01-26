9b852487fb346c99f745d5396fa0fdf0a9bf89e4
David Gassner <david.gassner@gmail.com>
2021-01-26 15:29:34 -0800

    02_05e

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum:", intSum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum:", floatSum)
+
+	roundedSum := math.Round(floatSum)
+	fmt.Println("Rounded sum:", roundedSum)
 
 }
