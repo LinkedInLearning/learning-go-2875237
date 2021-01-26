57eac5aebc6e6d4aba6068d0fbd4a3543ed6e07f
David Gassner <david.gassner@gmail.com>
2021-01-26 15:30:13 -0800

    02_06b

--- practice/main.go
+++ practice/main.go
@@ -7,10 +7,6 @@ import (
 
 func main() {
 
-	i1, i2, i3 := 12, 45, 68
-	intSum := i1 + i2 + i3
-	fmt.Println("Integer sum:", intSum)
-
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	floatSum := f1 + f2 + f3
 	fmt.Println("Float sum:", floatSum)
