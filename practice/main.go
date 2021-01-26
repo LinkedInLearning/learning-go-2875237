793e16a482a7d3394b658aa6dcaadaf3818c342b
David Gassner <david.gassner@gmail.com>
2021-01-26 15:38:41 -0800

    02_07b

--- practice/main.go
+++ practice/main.go
@@ -2,24 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/big"
 )
 
 func main() {
 
-	f1, f2, f3 := 23.5, 65.1, 76.3
-	floatSum := f1 + f2 + f3
-	fmt.Println("Float sum:", floatSum)
-
-	roundedSum := math.Round(floatSum)
-	fmt.Println("Rounded sum:", roundedSum)
-
-	var b1, b2, b3, bigSum big.Float
-	b1.SetFloat64(f1)
-	b2.SetFloat64(f2)
-	b3.SetFloat64(f3)
-	bigSum.Add(&b1, &b2).Add(&bigSum, &b3)
-	fmt.Printf("BigSum = %.10g\n", &bigSum)
+	fmt.Println("Dates and times")
 
 }
