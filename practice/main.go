8031e8800ffe705e29c10b21a21305ae54ed480f
David Gassner <david.gassner@gmail.com>
2021-01-29 12:45:09 -0800

    03_03b

--- practice/main.go
+++ practice/main.go
@@ -5,16 +5,5 @@ import (
 )
 
 func main() {
-	anInt := 42
-	var p = &anInt
-	fmt.Println("Value of p:", *p)
-
-	value1 := 42.13
-	pointer1 := &value1
-	fmt.Println("Value 1:", *pointer1)
-
-	*pointer1 = *pointer1 / 31
-	fmt.Println("Pointer 1:", *pointer1)
-	fmt.Println("Value 1:", value1)
-
+	fmt.Println("Arrays")
 }
