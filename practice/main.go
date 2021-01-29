6827891bd81595da7552a4701f0a16eae42d9be3
David Gassner <david.gassner@gmail.com>
2021-01-29 11:42:17 -0800

    03_02b

--- practice/main.go
+++ practice/main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-
-	n := time.Now()
-	fmt.Println("I recorded this video at ", n)
-
-	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
-	fmt.Println("Go launched at ", t)
-	fmt.Println(t.Format(time.ANSIC))
-
-	parsedTime, _ := time.Parse(time.ANSIC, "Tue Nov 10 23:00:00 2009")
-	fmt.Printf("The type of parsedTime is %T\n", parsedTime)
-
+	fmt.Println("Pointers")
 }
