6d601c0d0de7c663294d71f30a09e10c206eeaab
David Gassner <david.gassner@gmail.com>
2021-01-29 14:16:44 -0800

    04_01b

--- practice/main.go
+++ practice/main.go
@@ -5,16 +5,5 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
-	fmt.Println(poodle)
-	fmt.Printf("%+v\n", poodle)
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
-	poodle.Weight = 9
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
-}
-
-// Dog is a struct
-type Dog struct {
-	Breed  string
-	Weight int
+	fmt.Println("Conditional logic")
 }
