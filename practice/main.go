5e0f1cfdd53e22a9ae53ac9772a072544d64177c
David Gassner <david.gassner@gmail.com>
2021-01-29 12:55:37 -0800

    03_04b

--- practice/main.go
+++ practice/main.go
@@ -5,16 +5,6 @@ import (
 )
 
 func main() {
-	var colors [3]string
-	colors[0] = "Red"
-	colors[1] = "Green"
-	colors[2] = "Blue"
+	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
-	fmt.Println(colors[0])
-
-	var numbers = [5]int{5, 3, 1, 2, 4}
-	fmt.Println(numbers)
-
-	fmt.Println("Number of colors:", len(colors))
-	fmt.Println("Number of numbers:", len(numbers))
 }
