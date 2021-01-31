4877af92d619f972b4ba0ad623edaabbd747ad28
David Gassner <david.gassner@gmail.com>
2021-01-31 14:29:47 -0800

    04_02e

--- practice/main.go
+++ practice/main.go
@@ -8,6 +8,19 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+
+	// fmt.Println("Day", dow)
+
+	var result string
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "It's Sunday!"
+		// fallthrough
+	case 2:
+		result = "It's Monday!"
+		// fallthrough
+	default:
+		result = "It's some other day!"
+	}
+	fmt.Println(result)
 }
