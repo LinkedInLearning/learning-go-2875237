7e7e8a09da0c91785059fa2212ae88016b739f64
David Gassner <david.gassner@gmail.com>
2021-01-31 14:31:26 -0800

    04_03b

--- practice/main.go
+++ practice/main.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
-
-	// fmt.Println("Day", dow)
-
-	var result string
-	switch dow := rand.Intn(7) + 1; dow {
-	case 1:
-		result = "It's Sunday!"
-		// fallthrough
-	case 2:
-		result = "It's Monday!"
-		// fallthrough
-	default:
-		result = "It's some other day!"
-	}
-	fmt.Println(result)
+	colors := []string{"Red", "Green", "Blue"}
+	fmt.Println(colors)
 }
