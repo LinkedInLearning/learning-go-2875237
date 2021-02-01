94a9d496f375c2c51453423fce9bcd414ff3ec66
David Gassner <david.gassner@gmail.com>
2021-02-01 12:57:33 -0800

    04_03e

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,34 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	value := 1
+	for value < 10 {
+		fmt.Println("Value:", value)
+		value++
+	}
+
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("Sum:", sum)
+		if sum > 200 {
+			goto theEnd
+		}
+	}
+
+theEnd:
+	fmt.Println("End of program")
 }
