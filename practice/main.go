f24b158d7f5429258858ed0432467817fd4691a3
David Gassner <david.gassner@gmail.com>
2021-02-01 13:28:02 -0800

    05_02b

--- practice/main.go
+++ practice/main.go
@@ -9,8 +9,6 @@ func main() {
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
-	poodle.Weight = 9
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
 }
 
 // Dog is a struct
