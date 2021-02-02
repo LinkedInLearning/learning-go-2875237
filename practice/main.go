9181ca4bb9dfcf1cd1ff76016c1c112adf6a0cc9
David Gassner <david.gassner@gmail.com>
2021-02-02 10:09:02 -0800

    06_02e

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response type: %T\n", resp)
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(bytes)
+	fmt.Print(content)
 }
