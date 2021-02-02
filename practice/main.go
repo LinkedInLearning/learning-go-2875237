1a40854be668fc3fc136de6b2f4ee37919cebb84
David Gassner <david.gassner@gmail.com>
2021-02-02 10:41:27 -0800

    06_03e

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
@@ -25,5 +27,34 @@ func main() {
 	}
 
 	content := string(bytes)
-	fmt.Print(content)
+	// fmt.Print(content)
+
+	tours := toursFromJson(content)
+	for _, tour := range tours {
+		fmt.Println(tour.Name)
+	}
+}
+
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		if err != nil {
+			panic(err)
+		}
+		tours = append(tours, tour)
+	}
+	return tours
+}
+
+type Tour struct {
+	Name, Price string
 }
