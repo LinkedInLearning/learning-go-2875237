48dda1ea65edd4967dc7aef13aad927141c44e6e
David Gassner <david.gassner@gmail.com>
2021-02-02 10:42:17 -0800

    Initial commit

--- practice/main.go
+++ practice/main.go
@@ -2,28 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 )
 
-const url = "http://services.explorecalifornia.org/json/tours.php"
-
 func main() {
-
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Response type: %T\n", resp)
-
-	defer resp.Body.Close()
-
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	content := string(bytes)
-	fmt.Print(content)
+	fmt.Println("Hello from Go!")
 }
