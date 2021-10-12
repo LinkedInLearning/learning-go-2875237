0ed7d3f38117032b4bc4d5113748868514865ee6
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2021-10-13 02:20:29 +0500

    Store ordered values in arrays

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "red"
+	colors[1] = "green"
+	colors[2] = "blue"
+	fmt.Println(colors)
+	fmt.Println(colors[0])
+
+	var numbers = [5]int{5, 4, 3, 2, 1}
+	fmt.Println(numbers)
+
+	fmt.Println("Number of colors: ", len(colors))
+	fmt.Println("Number of numbers: ", len(numbers))
 }
