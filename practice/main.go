da193a4648b31102147e32120fe14f2bf97446ad
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2021-10-13 04:57:47 +0500

    Define the functions as methods of custom types

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,31 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Woof!"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
+	poodle.Sound = "Arf!"
+	poodle.Speak()
+	poodle.SpeakThreeTimes()
+	poodle.SpeakThreeTimes()
+
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+func (d Dog) SpeakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
