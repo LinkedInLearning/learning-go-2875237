f9e637190aeeb50d19fff7cd51d0d9b2727dccad
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2021-10-12 17:59:27 +0500

    Work with dates and times

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	n := time.Now()
+	fmt.Println("I recorded the video at ", n)
 
-	fmt.Println("Dates and times")
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Println("Go launched at ", t)
+	fmt.Println(t.Format(time.ANSIC))
 
+	parsedTime, _ := time.Parse(time.ANSIC, "Tue Nov 10 23:00:00 2009")
+	fmt.Println("The type of parseTime is %T\n", parsedTime)
 }
