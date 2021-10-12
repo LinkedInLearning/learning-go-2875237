3959bcbeefc9acd01d610c2d9fb9cab166d576d9
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2021-10-13 03:18:05 +0500

    Program conditional logic

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	theAnswer := 42
+	var result string
+
+	if theAnswer < 0 {
+		result = "Less than zero"
+	} else if theAnswer == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+
+	if x := -42; x < 0 {
+		result = "Less than zero"
+	} else if theAnswer == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
 }
