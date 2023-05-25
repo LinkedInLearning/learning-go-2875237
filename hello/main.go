8692a18e4fbd1e56e52361f7e31454a63b7dc203
Louis-sf <shufanwl@gmail.com>
2023-05-25 11:57:36 -0700

    revisit

--- hello/main.go
+++ hello/main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello from Shufan")
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
+	fmt.Println("Hello from GO!")
 }
