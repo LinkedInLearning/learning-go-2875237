2283d0eca0cad6fe6d896496840e9e5a8519a99a
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-30 20:32:52 +0100

    Store unordered values in maps

--- hello/main.go
+++ hello/main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	fmt.Println("Store unordered values in maps")
+	states := make(map[string]string)
+	fmt.Println(states)
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+	fmt.Println(states)
+
+	california := states["CA"]
+	fmt.Println(california)
+
+	//delete(states, "WA")
+	///fmt.Println(states)
+	for k,v := range states {
+		fmt.Printf("%v: %v:", k, v)
+	}
+	// extract the key as a slice to garanty the order
+	Keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		Keys[i] = k
+		i++
+	}
+	fmt.Println(Keys)
+	sort.Strings(Keys)
+	fmt.Println(Keys)
+
+	for i := range Keys {
+		fmt.Println(states[Keys[i]])
+	}
+
 }
