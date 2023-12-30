a8fb42b6ec4045810c1af55d7537569478fecae5
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-30 07:18:29 +0100

    ex7 done

--- hello/main.go
+++ hello/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	n := time.Now()
+	fmt.Println("I lunch this App At :", n)
+
+	t := time.Date(2023, time.December, 30, 7, 11, 0, 0, time.UTC)
+	fmt.Println("I lunch this App At :", t)
+	fmt.Println(t.Format(time.ANSIC))
+
+	parsedTime, _ := time.Parse(time.ANSIC, "Sat Dec 30 07:11:00 2023")
+	fmt.Printf("The type of parsedTime is %T\n", parsedTime)
 }
