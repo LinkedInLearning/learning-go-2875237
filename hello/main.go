904adafee5b88242c9c9fb77e8f9e7b3d735127a
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-30 13:47:42 +0100

    pointer

--- hello/main.go
+++ hello/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	intPointer := 45.31
+	// p is a pointer initialized with the address of intPointer
+	var p = &intPointer
+	fmt.Println("pointure", *p)
+	// p1 is a pointer to the address of intPointer
+	p1 := &intPointer
+	fmt.Println("pointure 2", *p1)
+
+	*p1 = *p1 / 31
+	fmt.Println("pointure 1:", *p1)
+	fmt.Println("pointure 2:", *p)
+	fmt.Println("value :", intPointer)
 }
