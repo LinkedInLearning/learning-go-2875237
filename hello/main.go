f3aa49cea6fc5cd9f500679fd0ccdc8b81a2f6b7
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-30 14:44:13 +0100

    Manage ordered values in slices

--- hello/main.go
+++ hello/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	var colors = []string{"Green", "red", "blue"} // now [] it's a slice , i can add new items
+	fmt.Println(colors)
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+	colors = append(colors[:len(colors)-1])
+	fmt.Print(colors, "\n")
+
+	numbers := make([]int, 5) //([]int, 5(number of items) , 5 (capacity that caps the number of items))
+	numbers[0] = 134
+	numbers[1] = 72
+	numbers[2] = 32
+	numbers[3] = 12
+	numbers[4] = 156
+	fmt.Println(numbers)
+	numbers = append(numbers, 235)
+	fmt.Println(numbers)
+	// sort slice by adding the sort package
+	sort.Ints(numbers)
+	fmt.Println(numbers)
+
 }
