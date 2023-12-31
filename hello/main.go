5cd250b96e5432602b47db862c27ddbaf2e88aac
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-31 05:54:23 +0100

    Program conditional logic

--- hello/main.go
+++ hello/main.go
@@ -5,5 +5,25 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	fmt.Println("Program conditional logic")
+	theAns := 42
+	var result string
+	if theAns < 0 {
+		result = "The result is less than 0"
+	} else if theAns == 0 {
+		result = "The result null "
+	} else {
+		result = "The result is Greater than 0"
+	}
+	fmt.Println(result)
+
+	if x := -42; x < 0 {
+		result = "The result is less than 0"
+	} else if x == 0 {
+		result = "The result null "
+	} else {
+		result = "The result is Greater than 0"
+	}
+	fmt.Println(result)
+
 }
