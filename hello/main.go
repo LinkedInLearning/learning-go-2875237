5350ed68f6a8ab89e6104b04fdbdf4abe69a2a2a
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-31 06:13:16 +0100

    Evaluate expressions with switch statements

--- hello/main.go
+++ hello/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	fmt.Println("Evaluate expressions with switch statements")
+	rand.Seed(time.Now().Unix())
+	dow := rand.Intn(7) + 1
+	fmt.Println("Day", dow)
+
+	var result string
+	switch dow {
+	case 1:
+		result = "Monday"
+	case 2:
+		result = "Tuesday"
+	default:
+		result = "it's another day"
+	}
+	fmt.Println(result)
 }
